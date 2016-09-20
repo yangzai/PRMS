3c3dbd320c576fc7eb5cf41e2810870092776657
hieu2608 <hieu2608@gmail.com>
2016-09-21 00:20:37 +0800

    Complete ManageScheduleCmd

--- src/java/sg/edu/nus/iss/phoenix/schedule/controller/ManageScheduleCmd.java
+++ src/java/sg/edu/nus/iss/phoenix/schedule/controller/ManageScheduleCmd.java
@@ -2,19 +2,30 @@ package sg.edu.nus.iss.phoenix.schedule.controller;
 
 import at.nocturne.api.Action;
 import at.nocturne.api.Perform;
+import sg.edu.nus.iss.phoenix.radioprogram.delegate.ReviewSelectProgramDelegate;
+import sg.edu.nus.iss.phoenix.radioprogram.entity.RadioProgram;
+import sg.edu.nus.iss.phoenix.schedule.delegate.ReviewSelectScheduledProgramDelegate;
+import sg.edu.nus.iss.phoenix.schedule.entity.ProgramSlot;
 
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.util.List;
 
 /**
- * Created by yao on 15/09/16.
+ * Created by Tran Ngoc Hieu 20 Sep 2016
+ * The ManageSchedule Command is triggered when user select Manage Schedule. It
+ * will get the list of program slot from Use Case ReviewSelectScheduledProgram
+ * and display it in scheduleProgramList page.
  */
 @Action("manageschedule")
 public class ManageScheduleCmd implements Perform {
     @Override
-    public String perform(String s, HttpServletRequest httpServletRequest, HttpServletResponse httpServletResponse) throws IOException, ServletException {
-        return null;
+    public String perform(String path, HttpServletRequest req, HttpServletResponse res) throws IOException, ServletException {
+        ReviewSelectScheduledProgramDelegate del = new ReviewSelectScheduledProgramDelegate();
+        List<ProgramSlot> data = del.reviewSelectScheduledProgram();
+        req.setAttribute("psl", data);
+        return "/pages/scheduleProgramList.jsp";
     }
 }
