8d544093bac8709a1d5508399b8c3a39b88db28b
hieu2608 <hieu2608@gmail.com>
2016-09-20 22:04:29 +0800

    Create new entity Presenter and Producer

---
+++ src/java/sg/edu/nus/iss/phoenix/user/entity/Presenter.java
@@ -0,0 +1,9 @@
+package sg.edu.nus.iss.phoenix.user.entity;
+
+import sg.edu.nus.iss.phoenix.authenticate.entity.User;
+
+/**
+ * Created by Tran Ngoc Hieu on 9/20/16.
+ */
+public class Presenter extends User{
+}
