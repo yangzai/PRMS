4fcc197c61d68bf656d8d780286b37f653277c0c
WangXuemin <e0013344@u.nus.edu>
2016-09-22 16:16:32 +0800

    modify scheduleProgramList.jsp and setupSchedule.jsp modify some commanda for communicating between pages and command

--- src/java/sg/edu/nus/iss/phoenix/user/controller/SelectPresenterProducerCmd.java
+++ src/java/sg/edu/nus/iss/phoenix/user/controller/SelectPresenterProducerCmd.java
@@ -1,7 +1,14 @@
 package sg.edu.nus.iss.phoenix.user.controller;
 
+import at.nocturne.api.Action;
+
 /**
  * Created by yao on 15/09/16.
  */
+/*
+* Changed by xuemin
+ */
+@Action("selectPresenterProducer")
 public class SelectPresenterProducerCmd {
+
 }
