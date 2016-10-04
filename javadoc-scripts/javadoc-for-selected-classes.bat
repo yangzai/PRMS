84ce8674a390ae4974403420e32f6ef6fb36cd88
Ang Hao Yang <yan9za1@gmail.com>
2016-10-04 12:03:42 +0800

    Added ant task. Updated selected classes for javadoc.

--- javadoc-scripts/javadoc-for-selected-classes.bat
+++ javadoc-scripts/javadoc-for-selected-classes.bat
@@ -4,5 +4,6 @@ REM by calling the javadoc-by-class script
 
 set base_path=%~dp0
 set base_path=%base_path:~0,-1%
-set classes=UserService ScheduleService
+set classes=UserService ReviewSelectUserService ReviewSelectPresenterProducerService^
+ ScheduleService ReviewSelectScheduledProgramService
 call "%base_path%\javadoc-by-class.bat" %classes%
--- javadoc-scripts/javadoc-for-selected-classes.sh
+++ javadoc-scripts/javadoc-for-selected-classes.sh
@@ -3,5 +3,6 @@
 # This script generates the javadocs for the required classes
 # by calling the javadoc-by-class script
 
-classes=(UserService ScheduleService)
+classes=(UserService ReviewSelectUserService ReviewSelectPresenterProducerService\
+ ScheduleService ReviewSelectScheduledProgramService)
 source "`dirname ${0}`/javadoc-by-class.sh" ${classes[*]}
